3f85509b18005c76b1ba35d2468967aa32fd4a0f
JorgeNoriegaR <84155064+JorgeNoriegaR@users.noreply.github.com>
2022-06-20 12:46:59 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let config = {
 	host: "us-cdbr-east-05.cleardb.net",
-	user: "b70c592033720c",
-	password: "c2783b1e",
+	user: "b70c592033720c"
+	password: "c2783b1e"
 	port: 3000,
 	database: "heroku_cb011285261ef28",
 	secret_key: "CruzAzulCampeonZzzzzzzzzz"
