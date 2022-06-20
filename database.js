fbd08bbe48d9b44a76eb1a0fa4d38b8ec4ab0d33
JorgeNoriegaR <84155064+JorgeNoriegaR@users.noreply.github.com>
2022-06-20 12:30:48 -0500

    Update database.js

--- database.js
+++ database.js
@@ -4,6 +4,6 @@ let config = {
 	password: "c2783b1e",
 	port: 3000,
 	database: "heroku_cb011285261ef28",
-	secret_key: "CruzAzulCampeon"
+	secret_key: "CruzAzulCampeonZzzzzzzzzz"
 };
 exports.config = config;
